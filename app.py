97d94785e2ec3ea23e9db7af197915bd33cfbbcf
Тарасова Елена <leeeeennaa@mail.ru>
2023-09-15 01:40:54 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Тарасова Елена Дмитриевна, Лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Елена Тарасова, ФБИ-12, 3 курс, 2023 
+        </footer>
+    <body>
+</html>
+"""
