41f2feccbd0a1b008419a1299563e114d7d811de
Тарасова Елена <leeeeennaa@mail.ru>
2023-09-15 09:37:37 +0700

    Меню

--- app.py
+++ app.py
@@ -2,7 +2,32 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <h1>Меню</h1>
+
+        <a href="http://127.0.0.1:5000/lab1" target="_blank">Первая лабораторная</a>
+
+        <footer>
+            &copy; Елена Тарасова, ФБИ-12, 3 курс, 2023 
+        </footer>
+    <body>
+</html>
+"""
+
+@app.route("/lab1")
+def lab1():
     return """
 <!doctype html>
 <html>
@@ -15,6 +40,11 @@ def start():
         </header>
 
         <h1>web-сервер на flask</h1>
+
+        <div>Flask — фреймворк для создания веб-приложений на языке
+        программирования Python, использующий набор инструментов
+        Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+        называемых микрофреймворков — минималистичных каркасов веб-приложений, сознательно предоставляющих лишь самые базовые возможности.</div><br>
 
         <footer>
             &copy; Елена Тарасова, ФБИ-12, 3 курс, 2023 
