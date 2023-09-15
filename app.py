8e1b82f7f986ee0984b31954d2eb5eedda655e97
Тарасова Елена <leeeeennaa@mail.ru>
2023-09-15 10:45:35 +0700

    Статистические файлы

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect 
+from flask import Flask, redirect, url_for 
 app = Flask(__name__)
 
 @app.route("/")
@@ -56,3 +56,13 @@ def menu():
     <body>
 </html>
 """
+@app.route ("/lab1/oak")
+def oak ():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>Дуб</h1>
+        <img src="'''+ url_for('static', filename='oak.jpg') +'''">    
+    </body>
+'''
