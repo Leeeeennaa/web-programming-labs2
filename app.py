ab15b0392f29bf52575ab32a28f4d66519336467
Тарасова Елена <leeeeennaa@mail.ru>
2023-09-15 09:51:28 +0700

    Перенаправление

--- app.py
+++ app.py
@@ -1,30 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def start():
-    return """
-<!doctype html>
-<html>
-    <head>
-        <title>НГТУ, ФБ, Лабораторные работы</title>
-    </head>
-    <body>
-        <header>
-            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
-        </header>
-
-        <h1>Меню</h1>
-
-        <a href="http://127.0.0.1:5000/lab1" target="_blank">Первая лабораторная</a>
-
-        <footer>
-            &copy; Елена Тарасова, ФБИ-12, 3 курс, 2023 
-        </footer>
-    <body>
-</html>
-"""
+    return redirect ("/menu", code = 302)
 
 @app.route("/lab1")
 def lab1():
@@ -45,6 +25,30 @@ def lab1():
         программирования Python, использующий набор инструментов
         Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
         называемых микрофреймворков — минималистичных каркасов веб-приложений, сознательно предоставляющих лишь самые базовые возможности.</div><br>
+
+        <footer>
+            &copy; Елена Тарасова, ФБИ-12, 3 курс, 2023 
+        </footer>
+    <body>
+</html>
+"""
+
+@app.route("/menu")
+def menu():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <h1>Меню</h1>
+
+        <a href="http://127.0.0.1:5000/lab1" target="_blank">Первая лабораторная</a>
 
         <footer>
             &copy; Елена Тарасова, ФБИ-12, 3 курс, 2023 
