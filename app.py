0847b3ccbf4a488aaa8ffb2e88d3cd86b9738976
Тарасова Елена <leeeeennaa@mail.ru>
2023-09-16 13:17:18 +0700

    Передача переменных в шаблон

--- app.py
+++ app.py
@@ -139,4 +139,8 @@ def Curtains():
 @app.route('/lab2/example')
 def example():
     name = 'Тейлор Свифт'
-    return render_template('example.html', name=name)
+    nomber = '2'
+    group = 'ФБИ-12'
+    course = '3 курс'
+    return render_template('example.html', name=name, nomber=nomber,group=group,course=course)
+
