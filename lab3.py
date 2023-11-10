e28db7ccc93548f515f52b7fbab7a97b8640d06e
Тарасова Елена <leeeeennaa@mail.ru>
2023-11-10 21:07:06 +0700

    Валидация

--- lab3.py
+++ lab3.py
@@ -9,7 +9,11 @@ def lab():
 
 @lab3.route('/lab3/form')
 def form1():
+    errors = {}
     user = request.args.get('user')
+    if user == '':
+        errors['user'] = 'Заполните поле!'
+
     age = request.args.get('age')
     sex = request.args.get('sex')
-    return render_template('form.html', user=user, age=age, sex=sex)
+    return render_template('form.html', user=user, age=age, sex=sex, errors=errors)
