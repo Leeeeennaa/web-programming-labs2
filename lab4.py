60a8e0bef9e62ee9070f94ade2db4b19814b1020
Тарасова Елена <leeeeennaa@mail.ru>
2023-11-25 03:42:57 +0700

    Авторизация

--- lab4.py
+++ lab4.py
@@ -5,3 +5,21 @@ lab4 = Blueprint('lab4',__name__)
 @lab4.route('/lab4/')
 def lab ():
     return render_template('lab4.html')
+
+
+@lab4.route('/lab4/login', methods = ['GET', 'POST'])
+def login(): 
+    if request.method == 'GET':
+        return render_template("login.html")
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username == 'Lena' and password == '123' :
+        return render_template('success4.html', username = username)
+    else:
+        error = 'Неверный логин и/или пароль'
+    if username == '':
+        error = 'Введите логин'
+    if password == '':
+        error = 'Введите пароль'
+    return render_template('login.html', error=error, username=username, password=password)
