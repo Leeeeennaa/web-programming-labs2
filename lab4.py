b0253d0ff223d3b47d1bacbbf3d1ef041c134d30
Тарасова Елена <leeeeennaa@mail.ru>
2023-11-25 05:37:25 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -23,3 +23,29 @@ def login():
     if password == '':
         error = 'Введите пароль'
     return render_template('login.html', error=error, username=username, password=password)
+
+
+
+@lab4.route('/lab4/fridge', methods=['GET', 'POST'])
+def fridge(): 
+    msg = ''
+    if request.method =='GET':
+        return render_template('fridge.html', msg=msg)
+    
+    temperature = request.form.get('temperature')
+
+    if temperature == '':
+        msg ='Не задана температура'
+    else:
+        temperature = int(temperature)
+        if temperature < -12:
+            msg ='Не удалось установить температуру — слишком низкое значение'
+        elif temperature > -1:
+            msg = 'Не удалось установить температуру — слишком высокое значение'
+        elif (temperature >= -12) and (temperature <= -9):
+            msg = f'Установлена температура: {temperature}°С'
+        elif (temperature >= -8) and (temperature <= -5):
+            msg = f'Установлена температура: {temperature}°С'
+        elif (temperature >= -4) and (temperature <= -1):
+            msg = f'Установлена температура: {temperature}°С'
+    return render_template('fridge.html', temperature=temperature, msg=msg)
