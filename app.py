dd4b4afab5e107148889efe4d6966514524a2cba
Тарасова Елена <leeeeennaa@mail.ru>
2023-11-25 01:33:34 +0700

    Заготовка к 4-ой лабораторной

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask, redirect, url_for, render_template
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from zadanie import zadanie
+from lab4 import lab4
+
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(zadanie)
+app.register_blueprint(lab4)
